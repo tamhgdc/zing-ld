bad1fa930425279b302b1224278d9d7d60ab9213
tamhgdc <108126724+tamhgdc@users.noreply.github.com>
2022-08-12 09:49:13 +0700

    Update FullScreen.jsx

--- src/components/FullScreen/FullScreen.jsx
+++ src/components/FullScreen/FullScreen.jsx
@@ -37,7 +37,7 @@ const FullScreen = () => {
   useEffect(() => {
     const getLyrics = async () => {
       await axios
-        .get(`https://musictes.vercel.app/api/lyric?id=${songId}`)
+        .get("https://musictes.vercel.app/api/lyric?id=${songId}")
         .then((res) => {
           dispatch(renderLyrics(res.data.data.sentences));
         })
